2b5fa0fd3f7ba5033feeb690b257cffc0399d77d
ptvung <whoiam9996@gmail.com>
2020-10-31 16:21:00 +0700

    update

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const DEFAULT_OPTIONS = {
     showCountryName = true,
 }
 
-export const CountryPicker = (props) => {
+export default CountryPicker = (props) => {
 
     const propsModel = {
         ...DEFAULT_OPTIONS,
@@ -19,4 +19,3 @@ export const CountryPicker = (props) => {
         <CountryPickerComponent {...propsModel} />
     );
 }
-
